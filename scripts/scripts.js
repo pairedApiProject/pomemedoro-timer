8091e04a49ec1fc17430b6a0066bbbffa5585409
hira <hiraahsan94@gmail.com>
2019-11-20 19:01:08 -0500

    initial setup comit, api call working :kitten:

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+const app = {};
+
+app.token = "-rMIOIVS06GhTaE1W8f4Y0pxOwqU";
+
+app.getToken = $.ajax({
+    url: 'https://oauth.reddit.com',
+    method: 'GET',
+    dataType: 'json',
+    headers: {
+        Authorization: `bearer ${app.token}`
+    }
+}).then(data => {
+    console.log(data);
+});
+
+app.getToken;
+
